6da2c0b2c8f383c6947bb69682e76e1a1df42250
thvmas29 <sabeltrblx@gmail.com>
2026-04-27 22:43:07 +0400

    Wrap App component with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css";
+import { HashRouter } from "react-router-dom"; // Add this
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
