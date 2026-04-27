dcc53aac594855767f34c501655ad5ffe4d1b7b2
thvmas29 <sabeltrblx@gmail.com>
2026-04-27 22:56:01 +0400

    Refactor root rendering logic with container check

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
-import { HashRouter } from "react-router"; 
 
 const container = document.getElementById("root");
 
-if (!container) {
-  throw new Error("Failed to find the root element. Check your index.html");
-}
-
-const root = createRoot(container);
-root.render(
-  <HashRouter>
+if (container) {
+  const root = createRoot(container);
+  root.render(
     <App />
-  </HashRouter>
-);
+  );
+}
