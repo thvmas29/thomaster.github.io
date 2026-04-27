3d00dbccca05810c42e76fe6fd19de03ea4020f6
thvmas29 <sabeltrblx@gmail.com>
2026-04-27 22:48:23 +0400

    Update HashRouter usage and improve rendering logic
    Refactor rendering logic to check for container existence before rendering.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
-import { HashRouter } from "react-router-dom"; // Add this
+import { HashRouter } from "react-router"; 
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
