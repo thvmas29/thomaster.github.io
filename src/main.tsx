1dfc1f108ddf75b6f59a1f3a66cd86db2baef779
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:54:53 +0400

    Update comments and import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/App' // On importe la constante router de App.tsx
+import { router } from './app/App' // Importe la constante router
 import './styles/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* C'est ce composant SEUL qui gère tout. Il ne faut rien d'autre. */}
+    {/* RouterProvider injecte le contexte de navigation partout d'un coup */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
