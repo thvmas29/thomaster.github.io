96f581f4ad61cb3a1fab098e11c0326a67caf412
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:28:41 +0400

    Integrate BrowserRouter with a base path
    Added BrowserRouter with a base path for routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Vérifie cette ligne
+import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 import './styles/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* On définit le chemin de base pour le routeur React */}
     <BrowserRouter basename="/thomaster.github.io">
       <App />
     </BrowserRouter>
