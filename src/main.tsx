ffc5df98d3d4b3de30196278a66002065c60fd8a
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:41:40 +0400

    Update comment on RouterProvider in main.tsx
    Clarified comment regarding RouterProvider usage.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* RouterProvider est le seul composant nécessaire ici */}
+    {/* RouterProvider doit être SEUL, il utilise la config de routes.ts */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
