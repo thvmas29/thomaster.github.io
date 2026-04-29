565eb1c274543a5b65bd4a89f7aae9c9afa2a8a5
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:17:26 +0400

    Refactor main.tsx to use BrowserRouter and StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './app/App'
+import './styles/index.css' // Ajuste le chemin si ton CSS est ailleurs
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* Le basename est la clé pour corriger l'erreur 404 sur GitHub Pages */}
+    <BrowserRouter basename="/thomaster.github.io">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
