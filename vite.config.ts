cc0d75d6a674ef2060513947d750205123bf38f8
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:41:57 +0400

    Simplify Vite config by consolidating plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // La base doit être identique au basename de ton main.tsx
   base: '/thomaster.github.io/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
 })
