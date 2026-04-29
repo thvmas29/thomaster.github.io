f78f2d2ae534c548047feb2528b2efde431b9f51
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:21:09 +0400

    Refactor imports and clean up comments in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // Vérifie cette ligne
 import App from './app/App'
-import './styles/index.css' // Ajuste le chemin si ton CSS est ailleurs
+import './styles/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Le basename est la clé pour corriger l'erreur 404 sur GitHub Pages */}
     <BrowserRouter basename="/thomaster.github.io">
       <App />
     </BrowserRouter>
