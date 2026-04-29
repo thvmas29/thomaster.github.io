4a698922ddc415892d5603fa3ff4f4ef8263b616
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:17:48 +0400

    Replace routing with useRoutes from react-router-dom
    Refactored App component to use useRoutes for routing.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,16 @@
-// Exemple dans ton main.tsx ou App.tsx
-<BrowserRouter basename="/thomaster.github.io">
-  <Routes>
-    <Route path="/" element={<Home />} />
-    {/* ... tes autres routes */}
-  </Routes>
-</BrowserRouter>
+import { useRoutes } from 'react-router-dom'
+// Importe tes routes depuis ton fichier routes.ts
+import { routes } from './routes' 
+
+function App() {
+  // Cette fonction va charger la bonne page en fonction de l'URL
+  const element = useRoutes(routes)
+
+  return (
+    <div className="app-container">
+      {element}
+    </div>
+  )
+}
+
+export default App
