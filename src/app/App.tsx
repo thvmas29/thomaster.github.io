f600efa5da7403220ac6e0269cfae5f58c1a9471
thvmas29 <sabeltrblx@gmail.com>
2026-04-30 00:01:21 +0400

    Refactor App component to use createBrowserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,38 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import { createBrowserRouter } from "react-router-dom";
+import { Root } from "./components/Root";
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
+import { SkillsPage } from "./pages/SkillsPage";
+import { InternshipsPage } from "./pages/InternshipsPage";
+import { ContactPage } from "./pages/ContactPage";
+import { ProjectsPage } from "./pages/ProjectsPage";
+import { ProjectDetailPage } from "./pages/ProjectDetailPage";
+import { BlogAppDocPage } from "./pages/BlogAppDocPage";
+import { VeillesPage } from "./pages/VeillesPage";
+import { TechWatchPage } from "./pages/TechWatchPage";
+import { LegalWatchPage } from "./pages/LegalWatchPage";
 
-export default function App() {
-  return <RouterProvider router={router} />;
-}
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Root,
+      children: [
+        { index: true, Component: HomePage },
+        { path: "about", Component: AboutPage },
+        { path: "skills", Component: SkillsPage },
+        { path: "internships", Component: InternshipsPage },
+        { path: "projets", Component: ProjectsPage },
+        { path: "projets/:projectId", Component: ProjectDetailPage },
+        { path: "projets/blog-app-cakephp/documentation", Component: BlogAppDocPage },
+        { path: "veilles", Component: VeillesPage },
+        { path: "veille-technologique", Component: TechWatchPage },
+        { path: "veille-juridique", Component: LegalWatchPage },
+        { path: "contact", Component: ContactPage },
+      ],
+    },
+  ],
+  {
+    basename: "/thomaster.github.io",
+  }
+);
