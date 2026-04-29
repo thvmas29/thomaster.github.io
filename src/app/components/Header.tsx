a537f862da60df2539989ebcf7886bbc98004a41
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 14:06:57 +0400

    Update import from 'react-router' to 'react-router-dom'

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Link, useLocation } from 'react-router';
+// ✅ Remplace par
+import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
 export function Header() {
