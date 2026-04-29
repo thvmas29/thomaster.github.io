c126709188d4bec7dfcfc3bb61121b629ebf2b02
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:34:46 +0400

    Replace BrowserRouter with RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './app/App'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/routes' // On importe ton router
 import './styles/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* On définit le chemin de base pour le routeur React */}
-    <BrowserRouter basename="/thomaster.github.io">
-      <App />
-    </BrowserRouter>
+    {/* On passe le router que tu as créé au Provider */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
