3a63291a81efdea45766a393e22a6bb6b2d8de56
thvmas29 <sabeltrblx@gmail.com>
2026-04-30 00:02:04 +0400

    Refactor main.tsx to use RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/App'
+import './styles/index.css'
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
