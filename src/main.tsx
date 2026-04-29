b5d1434fb232fbb0b457b9f25e5d2e9e9d36e6c6
thvmas29 <sabeltrblx@gmail.com>
2026-04-29 13:37:14 +0400

    Refine comments in main.tsx
    Updated comments for clarity on RouterProvider usage.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/routes' // On importe ton router
+import { router } from './app/routes' 
 import './styles/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* On passe le router que tu as créé au Provider */}
+    {/* RouterProvider doit être SEUL ici, car 'router' contient déjà la logique */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
